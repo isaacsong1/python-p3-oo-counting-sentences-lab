bbfd140da40fec401062ba18096641b984ae21fa
Isaac Song <isaacsongs10@gmail.com>
2023-10-25 20:39:56 -0700

    counting sentences lab complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=''):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+    
+  def is_sentence(self):
+    return True if self.value[-1] == '.' else False
+
+  def is_question(self):
+    return True if self.value[-1] == '?' else False
+  
+  def is_exclamation(self):
+    return True if self.value[-1] == '!' else False
+  
+  def count_sentences(self):
+    value = self.value
+    for punctuation in ['!', '?']:
+      value = value.replace(punctuation, '.')
+
+    return len([sent for sent in value.split('.') if sent])
+  
+
